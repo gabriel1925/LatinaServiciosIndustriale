6fe48c77432b3412895ae0c663f68ab0ec74038c
Gabriel Vauccassovitch <pocholo_1925@hotmail.com>
2019-12-03 13:14:24 -0300

    finalizando cambios en general

--- js/main.js
+++ js/main.js
@@ -10,9 +10,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(mymap);
 var marker = L.marker([lat,lon]).addTo(mymap);
 				marker.bindPopup(`<h5>Latina Servicios Industriales</h5>
-        Ruta provincial 88 · Parcela E Sector 2 <br>
+        <strong>Ruta provincial 88 · Parcela E Sector 2 <br>
         Parque Industrial Municipal <br>
         Luján de Cuyo · Mendoza · Argentina <br>
+        </strong>
         <a href="https://www.google.com/maps/dir//-33.0753999,-68.9706972/@-33.0754402,-68.9705794,18.79z/data=!4m2!4m1!3e0?hl=es-419" class="btn btn-warning" target="_blank">Como llegar</a>`).openPopup();
         console.log(marker);
         
